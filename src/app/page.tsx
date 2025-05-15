154c8cba176b9ef777d0f306833700f7d6531b81
elijahmccauley <emmccauley6@gmail.com>
2025-05-15 13:10:53 -0400

    fix image sizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
+"use client";
 import Image from "next/image";
+import ImageBackground from "next/image";
+// import { View, StyleSheet } from "react-native";
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+// import 'tailwindcss/tailwind.css';
 
 export default function Home() {
   const hours = [
@@ -10,20 +17,65 @@ export default function Home() {
     ["Saturday", "Closed"],
     ["Sunday", "Closed"],
   ];
+  const work = [
+    ["/work1.jpg", "work1"],
+    ["/work2.jpg", "work2"],
+    ["/work3.jpg", "work3"],
+  ]
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 2) % 3);
+  };
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % 3);
+  };
   return (
-    <div className="min-h-screen bg-[#8A1118] p-6 sm:p-20 font-[family-name:var(--font-geist-sans)] text-white">
-      <main className="max-w-4x1 mx-auto flex flex-col items-center sm:items-start gap-16">
-        <Image
-          className="dark:invert w-full"
-          src="/brownleeautomotive.png"
-          alt="brownlee_logo"
-          width={220}
-          height={40}
-          priority
-        />
-        <section className="text-sm font-[family-name:var(--font-geist-mono)] text-center">
-          <p className="tracking-tight text-center">Family owned and operated since 1987</p>
+    <div className="min-h-screen bg-[#8A1118] p-6 sm:p-20 font-[family-name:var(--font-geist-sans)] text-white text-center">
+      <main className="max-w-8xl mx-auto flex flex-col items-center gap-8">
+        <section className="sm:text-8xl text-4xl font-[family-name:var(--font-geist-mono)] text-center items-center">
+          <p className="tracking-tight text-center">BROWNLEE AUTOMOTIVE</p>
         </section>
+
+        <section className="text-xl font-sans items-center">
+          <p className="tracking-tight text-center">Family owned and operated since 1987</p>
+          <p className="tracking-tight text-center">We work on all makes and models</p>
+        </section>
+
+        <section className="w-full flex flex-col items-center">
+          <div className="w-full relative">
+            <button
+              onClick={handlePrev}
+              className={`absolute left-4 top-1/2 transform -translate-y-1/2 px-4 py-2 bg-gray text-white rounded disabled:opacity-50 z-10`}
+            >
+            <FontAwesomeIcon icon={faArrowLeft} className="lg" />
+            </button>
+        
+            {work.slice(currentIndex, currentIndex + 1).map(([src, alt]) => (
+              <div key={alt} className="w-full">
+                <div className="aspect-w-16 aspect-h-9">
+                <Image
+                  key={alt}
+                  className="dark:invert"
+                  src={src}
+                  alt={alt}
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  style={{ width: '100%', height: 'auto' }}
+                  priority
+                />
+                </div>
+              </div>
+            ))}
+            <button
+              onClick={handleNext}
+              className={`absolute right-4 top-1/2 transform -translate-y-1/2 px-4 py-2 bg-gray text-white rounded disabled:opacity-50 z-10`}
+            >
+            <FontAwesomeIcon icon={faArrowRight} className="lg" />
+            </button>
+          </div>
+        </section>
+
         <section className="text-sm font-[family-name:var(--font-geist-mono)] text-center sm:text-left">
           <p className="tracking-tight">📞 +1 (770) 469-9660</p>
           <p className="tracking-tight">📍 6573 James B Rivers Memorial Dr, Stone Mountain, GA 30083</p>
@@ -59,7 +111,7 @@ export default function Home() {
         </section>
         <section className="w-full text-black text-sm grid grid-cols-1 sm:grid-cols-2 gap-4">
           <p className="bg-white rounded shadow-lg leading-relaxed text-sm p-6">I recently had my truck's transmission repaired at Brownlee Automotive, and I couldn't be happier with the service. Brett, the service technician, was extremely knowledgeable and efficient, ensuring the repair was done right the first time. I highly recommend Brownlee Automotive for their excellent work and professional staff.</p>
-          <p className="bg-white rounded shadow-lg leading-relaxed text-sm p-6">Dan has something to say probably</p>
+          <p className="bg-white rounded shadow-lg leading-relaxed text-sm p-6">RL and Brett at Brownlee Automotive have kept my 2004 Honda Oddysey running for over 380 thousand miles</p>
         </section>
         <div className="relative w-full max-w-md mx-auto">
           <input
